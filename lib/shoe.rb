e32cddcf56314d436d13e3bad3d88d8b797f1b08
Michael Dobak <mdobak44@icloud.com>
2021-02-19 22:42:17 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,13 +6,8 @@ class Shoe
  
   def initialize(brand)
     @brand = brand
-    # BRANDS << brand unless BRANDS.include?(brand)
-    BRANDS << brand
+    BRANDS << brand unless BRANDS.include?(brand)
   end
-  
-  def brand
-  end
-  
 
   def cobble
     self.condition = "new"
