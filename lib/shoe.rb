1ef0074494efbed5ff0b208d3c789890259b511b
Michael Dobak <mdobak44@icloud.com>
2021-02-19 22:30:41 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,6 +6,7 @@ class Shoe
  
   def initialize(brand)
     @brand = brand
+    BRANDS << brand
   end
   
 
@@ -14,8 +15,5 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
-  def brand=(brand)
-    @brand = brand
-    BRANDS << brand
-  end
+  
 end
